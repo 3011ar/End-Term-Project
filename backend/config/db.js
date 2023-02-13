f48a2877baa18a8039e5506d565bbd08119cbbe6
SAHIL RAYU <93898251+3011ar@users.noreply.github.com>
2023-02-13 22:45:42 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb+srv://root:root@cluster0.yl8dqjf.mongodb.net/bloodbank?retryWrites=true&w=majority", {
+    const conn = await mongoose.connect("mongodb+srv://<User>:<Password>@id", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
